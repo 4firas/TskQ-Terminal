8160cc1a9e1061b96f38c228898cf36b3202668a
Tsk <92300520+TskQ@users.noreply.github.com>
2026-01-31 20:40:02 +0400

    Update website description in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "(¬‿¬)",
-  description: "Windows XP themed personal website by tskq",
+  description: "My one and only website~",
   generator: "TskQ",
 }
 
